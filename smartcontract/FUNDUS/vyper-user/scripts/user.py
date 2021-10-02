cb1a916d85726c7a82e4432f2e7f5bbca88e75ae
Frank Beutelschiess <frank@thesym.site>
2021-10-02 22:05:47 +0200

    add stuff from proto: 5flaskVyper

---
+++ smartcontract/FUNDUS/vyper-user/scripts/user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# from brownie import Token, accounts
+import brownie
+
+
+def main():
+    return brownie
+    # return brownie.Token.deploy("Test Token", "TST", 18, 1e21, {'from': accounts[0]})
